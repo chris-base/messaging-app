079a4b5aba5b8d459e387843cf330f704ef16b03
chris-base <baseappsdev@gmail.com>
2022-04-18 20:29:32 -0500

    started sidebar layout

--- src/Components/SidebarComponent.js
+++ src/Components/SidebarComponent.js
@@ -1,9 +1,21 @@
 import "../Styles/SidebarStyles.css";
 
 const SidebarComponent = () => {
+  let channels = ["Main channel", "second channel"];
+
   return (
     <div id='sidebarContainer'>
-      <div></div>
+      <div id='sidebarHeader'>
+        <h1>Messaging App</h1>
+      </div>
+
+      {[...channels].map((channel, index) => {
+        return (
+          <div id='channelContainer' key={index}>
+            <p id='channelTitle'>#{channel}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
