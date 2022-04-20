f790c80634303646e4172003347c480b845a5510
chris-base <baseappsdev@gmail.com>
2022-04-20 18:29:51 -0500

    added sidebar button icons/more sidebar layout advancements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import SidebarComponent from "./Components/SidebarComponent";
 import MainChatComponent from "./Components/MainChatComponent";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <div className='App'>
-      <SidebarComponent />
+      <SidebarComponent darkMode={darkMode} setDarkMode={setDarkMode} />
       <MainChatComponent />
     </div>
   );
--- src/Components/SidebarComponent.js
+++ src/Components/SidebarComponent.js
@@ -1,26 +1,37 @@
 import "../Styles/SidebarStyles.css";
 
-const SidebarComponent = () => {
+const SidebarComponent = ({ darkMode, setDarkMode }) => {
   let channels = ["Main channel", "second channel"];
 
   return (
     <div id='sidebarContainer'>
-      <div id='sidebarHeader'>
-        <h1>Messaging App</h1>
+      <div id='mainSBContents'>
+        <div id='sidebarHeader'>
+          <h1>Messaging App</h1>
+        </div>
+
+        {[...channels].map((channel, index) => {
+          return (
+            <div id='channelContainer' key={index}>
+              <p id='channelTitle'>#{channel}</p>
+            </div>
+          );
+        })}
       </div>
 
-      {[...channels].map((channel, index) => {
-        return (
-          <div id='channelContainer' key={index}>
-            <p id='channelTitle'>#{channel}</p>
-          </div>
-        );
-      })}
-
       <div id='sidebarButtonContainer'>
-        <div id='darkModeButton' />
+        <div
+          onClick={() => setDarkMode(!darkMode)}
+          class='sbButtons'
+          id='darkModeButton'
+          style={darkMode ? { filter: "invert(100%) sepia(53%) saturate(353%) hue-rotate(240deg) brightness(107%) contrast(102%)" } : {}}
+        />
 
-        <div id='logoutButton' />
+        <div
+          class='sbButtons'
+          id='logoutButton'
+          style={darkMode ? { filter: "invert(100%) sepia(53%) saturate(353%) hue-rotate(240deg) brightness(107%) contrast(102%)" } : {}}
+        />
       </div>
     </div>
   );
